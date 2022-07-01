4e1d524271d79ea46cd863a44c1b0a417dea2de9
Dusty Rowe <dusty.rowe@frogslayer.com>
2022-07-01 12:20:53 -0500

    Fix reference path

--- src/harvest/ProjectTargets.jsx
+++ src/harvest/ProjectTargets.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import useScript from 'hooks/useScript';
+import useScript from '@/hooks/useScript';
 
 function ProjectTargets({ match }) {
     const { path } = match;
